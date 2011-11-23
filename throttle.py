002cb5aff70333f15ee9ed407a8e5d91d72732ea
Fred Blau <fredblau@gmail.com>
2011-11-22 21:47:22 -0500

    Make a function call for each timeslice.  Also tweak timing calculation

--- throttle.py
+++ throttle.py
@@ -3,6 +3,35 @@
 import sys
 import time
 
+BLOCK_READ_SIZE = 1024 #larger size = less cpu usage
+
+
+def write_one_timeslice(readfile, writefile, bytes_to_send, old_leftover_data):
+    bytes_sent = 0
+    new_leftover_data = None
+    if old_leftover_data:
+        data_to_write = old_leftover_data[:bytes_to_send]
+        new_leftover_data = old_leftover_data[bytes_to_send:]
+        writefile.write(data_to_write)
+        bytes_sent = len(data_to_write)
+    while bytes_sent < bytes_to_send:
+        data = readfile.read(BLOCK_READ_SIZE)
+        data_len = len(data)
+        if data_len == 0:
+            return (True, None) #is_done, leftover_data
+        chunk = bytes_to_send-bytes_sent
+        if chunk >= data_len:
+            data_to_write = data
+            new_leftover_data = (False, None)
+        else:
+            data_to_write = data[:chunk]
+            new_leftover_data = data[chunk:]
+            data_len = chunk
+        writefile.write(data_to_write)
+        bytes_sent += data_len
+    return (False, new_leftover_data)
+
+
 def main():
     total = 0
     #speed = 1048576
@@ -10,31 +39,23 @@ def main():
     start_time = time.time()
     super_start_time = start_time
     bytes_this_chunk = 0
-    while True:
-        #start_time = time.time()
-        v = sys.stdin.read(1024)
-        cur_len = len(v)
-        bytes_this_chunk += cur_len
-        total += cur_len
-        sys.stdout.write(v)
-        #print >> sys.stderr, len(v), total
-        if cur_len == 0:
-            print >> sys.stderr, 'done?'
-            return
-        if bytes_this_chunk > bytes_per_chunk:
-            cur_time = time.time()
-            #print >> sys.stderr, 1.0*bytes_this_chunk/(cur_time-start_time)
-            time_so_far = cur_time - start_time
-            if time_so_far >= 0.01:
-                pass
-            else:
-                time.sleep(0.01 - time_so_far)
-                #newt=time.time()
-            start_time = time.time()
-            start_time = cur_time + 0.01 - time_so_far
-            bytes_this_chunk = 0
+    is_done = False
+    old_leftover_data = None
+    while not is_done:
+        is_done, leftover_data = write_one_timeslice(sys.stdin, sys.stdout, bytes_per_chunk, old_leftover_data)
+        #ok, we've written the right amount of data for our timeslice, now how much time has passed?
+        cur_time = time.time()
+        time_so_far = cur_time - start_time
+        if time_so_far >= 0.01:
+            #print >> sys.stderr, "more", time_so_far
+            pass
+        else:
+            #print >> sys.stderr, "less", time_so_far
+            time.sleep(0.01 - time_so_far)
+        #start_time = cur_time + 0.01 - time_so_far #works even if we go over.
+        start_time = start_time + 0.01 #what we really want
 
-
+    print >> sys.stderr, 'done?'
 
 
 if __name__ == '__main__':
